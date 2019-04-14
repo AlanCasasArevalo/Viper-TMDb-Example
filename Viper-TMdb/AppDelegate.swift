1d6de9402ffb7f3c9e69ab2182445170205926cf
Alan Casas <alancasasarevalo@gmail.com>
2019-04-14 07:38:35 +0200

    [FEATURE][STORYBOARDS] - Storyboards were deleted

---
+++ Viper-TMdb/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Viper-TMdb
+//
+//  Created by Alan Casas on 14/04/2019.
+//  Copyright © 2019 Alan Casas. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = UIColor.cyan
+        
+        window?.rootViewController = UIViewController()
+     
+        return true
+    }
+
+}
+
