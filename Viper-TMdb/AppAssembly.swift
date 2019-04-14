c7cb3291e0796fad73dac80c8b9b73919161bf26
Alan Casas <alancasasarevalo@gmail.com>
2019-04-14 07:45:28 +0200

    [FEATURE][APP ASSEMBLY] - App and core assembly were created to contain clean architecture

---
+++ Viper-TMdb/AppAssembly.swift
@@ -0,0 +1,15 @@
+//
+//  AppAssembly.swift
+//  Viper-TMdb
+//
+//  Created by Alan Casas on 14/04/2019.
+//  Copyright © 2019 Alan Casas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class AppAssembly {
+    private (set) lazy var coreAssembly = CoreAssembly()
+    private (set) lazy var window = UIWindow(frame: UIScreen.main.bounds)
+}
