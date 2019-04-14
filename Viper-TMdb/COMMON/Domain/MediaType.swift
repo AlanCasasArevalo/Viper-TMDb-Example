a1c0f2497b8f52a29ab7faed55725e6cc565dd94
Alan Casas <alancasasarevalo@gmail.com>
2019-04-14 07:59:24 +0200

    [FEATURE][MODELS] - Models were added

---
+++ Viper-TMdb/COMMON/Domain/MediaType.swift
@@ -0,0 +1,15 @@
+//
+//  MediaType.swift
+//  Viper-TMdb
+//
+//  Created by Alan Casas on 14/04/2019.
+//  Copyright © 2019 Alan Casas. All rights reserved.
+//
+
+import Foundation
+
+enum MediaType {
+    case movie
+    case show
+    case person
+}
